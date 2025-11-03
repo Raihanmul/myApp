eda1e06081c2e3fca4c7a3f58eede18811870450
Raihanmul <raihanmul10@gmail.com>
2025-11-03 19:39:30 +0700

    empty data homescreen

--- components/tugas/HomeScreen.tsx
+++ components/tugas/HomeScreen.tsx
@@ -11,34 +11,34 @@ import {
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const notes: Note[] = [
-  {
-    id: 1,
-    image: require("@/assets/images/image-1.png"),
-    title: "Belajar Mobile App",
-    description: "Belajar membuat aplikasi mobile app",
-    date: "29 Oktober 2025",
-  },
-  {
-    id: 2,
-    image: require("@/assets/images/image-2.png"),
-    title: "Belajar Backend",
-    description: "Belajar membuat restful api",
-    date: "30 Oktober 2025",
-  },
-  {
-    id: 3,
-    image: require("@/assets/images/image-3.png"),
-    title: "Belajar Backend",
-    description: "Belajar membuat restful api",
-    date: "30 Oktober 2025",
-  },
-  {
-    id: 4,
-    image: require("@/assets/images/image-4.png"),
-    title: "Belajar Backend",
-    description: "Belajar membuat restful api",
-    date: "30 Oktober 2025",
-  },
+  // {
+  //   id: 1,
+  //   image: require("@/assets/images/image-1.png"),
+  //   title: "Belajar Mobile App",
+  //   description: "Belajar membuat aplikasi mobile app",
+  //   date: "29 Oktober 2025",
+  // },
+  // {
+  //   id: 2,
+  //   image: require("@/assets/images/image-2.png"),
+  //   title: "Belajar Backend",
+  //   description: "Belajar membuat restful api",
+  //   date: "30 Oktober 2025",
+  // },
+  // {
+  //   id: 3,
+  //   image: require("@/assets/images/image-3.png"),
+  //   title: "Belajar Backend",
+  //   description: "Belajar membuat restful api",
+  //   date: "30 Oktober 2025",
+  // },
+  // {
+  //   id: 4,
+  //   image: require("@/assets/images/image-4.png"),
+  //   title: "Belajar Backend",
+  //   description: "Belajar membuat restful api",
+  //   date: "30 Oktober 2025",
+  // },
 ];
 
 type Note = {
